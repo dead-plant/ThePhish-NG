ee3e2eeaef093801cc76d8d90c099efcd9ea4031
Alexander Stocker <contact@a-stocker.com>
2026-07-07 10:25:09 +0200

    add config option to choose imap tls mode

--- app/utils/imap.py
+++ app/utils/imap.py
@@ -7,10 +7,11 @@ from utils.ws_logger import WebSocketLogger
 
 
 def connect(config: dict, log: logging.Logger, wsl: Optional[WebSocketLogger] = None) -> imaplib.IMAP4_SSL | imaplib.IMAP4:
-	timeout = 15
+	timeout = 5
 
 	host = config['imap']['host']
 	port = config['imap']['port']
+	tls = config['imap']['tls']
 	insecure = config['imap']['tlsinsecure']
 	user = config['imap']['user']
 	pwd = config['imap']['password']
@@ -25,19 +26,26 @@ def connect(config: dict, log: logging.Logger, wsl: Optional[WebSocketLogger] = 
 		raise Exception("insecure must be 'yes' or 'no'")
 
 	# Prefer implicit TLS (IMAP over SSL)
-	try:
+	if tls == "tls":
 		conn = imaplib.IMAP4_SSL(host, port, ssl_context=ctx, timeout=timeout)
 		conn.login(user, pwd)
-		log.info('Connected to {0}@{1}:{2}/{3} using implicit tls. insecure={4}'.format(user, host, port, folder, insecure))
+		log.info('Connected to {0}@{1}:{2}/{3} using implicit tls. insecure={4}'.format(user, host, port, folder, tls, insecure))
 		if wsl is not None:
-			wsl.emit_info('Connected to email {0} server {1}:{2}/{3} using implicit tls. insecure={4}'.format(user, host, port, folder, insecure))
+			wsl.emit_info('Connected to email {0} server {1}:{2}/{3} using implicit tls. insecure={4}'.format(user, host, port, folder, tls, insecure))
 		return conn
-	except Exception as e_tls:
-		# Fallback to STARTTLS
+	elif config['imap']['tls'] == "starttls":
 		conn = imaplib.IMAP4(host, port, timeout=timeout)
 		conn.starttls(ssl_context=ctx)
 		conn.login(user, pwd)
-		log.info('Connected to {0}@{1}:{2}/{3} using Starttls. insecure={4}'.format(user, host, port, folder, insecure))
+		log.info('Connected to {0}@{1}:{2}/{3} using Starttls. insecure={4}'.format(user, host, port, folder, tls, insecure))
 		if wsl is not None:
-			wsl.emit_info('Connected to email {0} server {1}:{2}/{3} using Starttls. insecure={4}'.format(user, host, port, folder, insecure))
-		return conn
+			wsl.emit_info('Connected to email {0} server {1}:{2}/{3} using Starttls. insecure={4}'.format(user, host, port, folder, tls, insecure))
+	elif config['imap']['tls'] == "none":
+		conn = imaplib.IMAP4(host, port, timeout=timeout)
+		conn.login(user, pwd)
+		log.info('Connected to {0}@{1}:{2}/{3} using Starttls. insecure={4}'.format(user, host, port, folder, tls, insecure))
+		if wsl is not None:
+			wsl.emit_info('Connected to email {0} server {1}:{2}/{3} using Starttls. insecure={4}'.format(user, host, port, folder, tls, insecure))
+	else:
+		raise Exception("imap.tls must be 'tls' or 'starttls' or 'none'")
+	return conn
