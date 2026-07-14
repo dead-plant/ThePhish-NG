99c656caee29898504dcfaae30055f4828479bc2
Alexander Stocker <contact@a-stocker.com>
2026-07-14 10:05:41 +0200

    create files thehive.py and cortex.py

---
+++ app/repositories/cortex.py
@@ -0,0 +1,7 @@
+import logging
+import cortex4py
+
+from app import config
+
+log = logging.getLogger(__name__)
+
---
+++ app/repositories/thehive.py
@@ -0,0 +1,7 @@
+import logging
+import thehive4py
+
+from app import config
+
+log = logging.getLogger(__name__)
+
