de1320181a96ab17fabf03405f788496791ee576
Alexander Stocker <contact@a-stocker.com>
2026-07-15 11:09:46 +0200

    improve safelink decoding utility

--- app/utils/safelink_decoder.py
+++ app/utils/safelink_decoder.py
@@ -8,8 +8,9 @@ import urllib.parse
 
 log = logging.getLogger(__name__)
 
+MAX_DEPTH: Final[int] = 5
+
 SAFE_LINK_DOMAIN_PATTERN: Final = re.compile(r'\.safelinks\.protection\.outlook\.com$', re.IGNORECASE)
-SAFE_LINK_URL_PARAM_PATTERN: Final = re.compile(r'[?&]url=([^&]+)', re.IGNORECASE)
 
 class SafeLinkDecodingError(Exception):
     """Raised when decoding a safe link fails."""
@@ -18,6 +19,19 @@ class SafeLinkDecodingError(Exception):
 class NotASafeLinkError(SafeLinkDecodingError):
     """Raised when decoding a URL that isn't a safe link."""
 
+
+class DecodingMaxDepthError(SafeLinkDecodingError):
+    """Raised when a safelink passes the max depth limit."""
+    def __init__(self, reached_safelink: str) -> None:
+        self.url = reached_safelink
+        super().__init__(f"Reached max depth of {MAX_DEPTH} without finding the url.")
+
+
+class LoopingSafeLinkError(SafeLinkDecodingError):
+    """Raised when decoding a safelink returns a previous safelink."""
+    def __init__(self, looping_safelink: str) -> None:
+        self.url = looping_safelink
+        super().__init__("Found looping safelink.")
 
 def is_safelink(url: str) -> bool:
     """
@@ -37,10 +51,10 @@ def is_safelink(url: str) -> bool:
 
     host = urllib.parse.urlparse(url).hostname
     if host is None:
-        log.debug("URL %r has no hostname and is not a Safelink", host)
+        log.debug("URL %r has no hostname and is not a Safelink", url)
         return False
 
-    is_safe_link = bool(SAFE_LINK_DOMAIN_PATTERN.search(host.strip()))
+    is_safe_link = bool(SAFE_LINK_DOMAIN_PATTERN.search(host))
     log.debug("Checked if host %r is a Safelink: %s", host, is_safe_link)
     return is_safe_link
 
@@ -57,27 +71,38 @@ def decode_safelink(safelink: str) -> str:
             ValueError: If the passed url is None or empty
             NotASafeLinkError: If the URL is not a Safelink
             SafeLinkDecodingError: If decoding fails
+            DecodingMaxDepthError: If the safelink passes the max depth limit.
+            LoopingSafeLinkError: If decoding a safelink returns a previous safelink.
     """
     if not is_safelink(safelink):
         raise NotASafeLinkError("Passed url is not a Safelink")
 
     log.debug("Decoding Safelink %r", safelink)
     url = safelink.strip()
+    history = [url]
     depth = 0
 
     while is_safelink(url):
-        if depth >= 5:
-            raise SafeLinkDecodingError("Reached max depth of 5 without finding the original url.")
-        
-        match = SAFE_LINK_URL_PARAM_PATTERN.search(url)
-        if match:
-            try:
-                encoded_url = match.group(1)
-                url = urllib.parse.unquote(encoded_url)
-            except Exception as exc:
-                raise SafeLinkDecodingError("Couldn't decode url parameter.") from exc
+        if depth >= MAX_DEPTH:
+            raise DecodingMaxDepthError(url)
+
+        try:
+            url_params = urllib.parse.parse_qs(urllib.parse.urlparse(url).query).get("url")
+        except Exception as exc:
+            raise SafeLinkDecodingError("Couldn't decode the safelink.") from exc
+
+        if not url_params or len(url_params) != 1:
+            count = len(url_params) if url_params else 0
+            raise SafeLinkDecodingError(f"Expected exactly 1 url parameter, got {count}")
+        url = url_params[0]
+
+        if not url.strip():
+            raise SafeLinkDecodingError("Got empty url.")
+
+        if url in history:
+            raise LoopingSafeLinkError(url)
         else:
-            raise SafeLinkDecodingError("Invalid safelink: Link has no url parameter.")
+            history.append(url)
 
         depth += 1
         log.debug("Decoded Safelink layer %d to %r", depth, url)
