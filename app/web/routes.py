2c6dc2df1a5407aa01a3932a6921e917508e3a35
Alexander Stocker <contact@a-stocker.com>
2026-07-20 13:13:40 +0200

    load navbar hrefs from config

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 
 from app.web import web_bp
+from app import config
 
 # The main page
 @web_bp.route("/")
 def index():
-    return render_template("index.html")
+    webapp = config.get_app_config()["webapp"]
+    thehive_url = webapp.get("thehive_url")
+    cortex_url = webapp.get("cortex_url")
+    misp_url = webapp.get("misp_url")
+
+    return render_template("index.html", webapp_thehive_url=thehive_url, webapp_cortex_url=cortex_url, webapp_misp_url=misp_url)
