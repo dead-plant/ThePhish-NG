1312248b4fa19dd2f61595835084a9ccb3ae984a
Alexander Stocker <contact@a-stocker.com>
2026-07-21 09:27:50 +0200

    api layout

--- app/api/routes.py
+++ app/api/routes.py
@@ -43,20 +43,25 @@ def list_emails():
     return jsonify(listing.list_emails())
 
 
-@api_bp.route("/analysis", methods=["POST"])
+@api_bp.route("/analyses", methods=["POST"])
 def create_analysis():
     # start analysis
     ...
 
-"""
-@api_bp.route("/analysis", methods=["GET"])
-def list_analysis():
-    # list all open cases
+
+@api_bp.route("/analyses/<analysis_id>", methods=["GET"])
+def get_analysis(analysis_id):
+    # get the status of an analysis
     ...
 
 
-@api_bp.route("/analysis/<case_id>", methods=["GET"])
-def get_analysis(case_id):
-    # get details of a analysis
+@api_bp.route("/analyses/<analysis_id>/log", methods=["GET"])
+def get_analysis_log(analysis_id):
+    # get the log of an analysis
     ...
-"""
+
+
+@api_bp.route("/analyses/<analysis_id>/stream", methods=["GET"])
+def stream_analysis(analysis_id):
+    # live log of an ongoing analysis
+    ...
